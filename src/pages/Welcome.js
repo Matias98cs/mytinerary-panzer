84e8ad388b395f05e0c3d8d8a58567533deac296
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-08-19 10:00:54 -0300

    Agregamos titulo

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../style/Welcome.css'
 import MyLogo from '../images/logo.png'
+import MyLogo2 from '../images/avion.png'
 
 export default function Welcome() {
   return (
     <div className='Welcome-container'>
+        <h1 className='Welcome-title'><span>M</span>y <span>T</span>inerary</h1>
         <div className='Welcome-title-logo'>
             <img src={MyLogo} alt="" />
-            <h3>Find your perfect trip, desingned by insiders who know and love their cities!"</h3>
+            <h3>Find your perfect trip, designed by insiders who know and love their cities!"</h3>
         </div>
+        
         <button>Login</button>
     </div>
   )
