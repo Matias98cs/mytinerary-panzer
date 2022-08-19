069a50e813c9065e89750dc19a2014ce19dc64e6
Matias98cs <matias98cs@gmail.com>
2022-08-19 15:03:55 -0300

    agregamos el componente de carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './style/App.css';
 import Footer from './componets/Footer';
 import Welcome from './pages/Welcome';
+import Carousel from './componets/Carousel';
 
 
 function App() {
   return (
     <>
+      <Carousel />
       <Welcome/>
       <Footer/>
     </>
