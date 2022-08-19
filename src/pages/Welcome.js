a41a8a51d69834ed5ec371b1829ae5e1a93fec3e
Matias98cs <matias98cs@gmail.com>
2022-08-19 13:55:13 -0300

    cambios las rutas de img de welcome

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import '../style/Welcome.css'
-import MyLogo from '../images/logo.png'
-import MyLogo2 from '../images/avion.png'
 
 export default function Welcome() {
   return (
     <div className='Welcome-container'>
         <h1 className='Welcome-title'><span>M</span>y <span>T</span>inerary</h1>
         <div className='Welcome-title-logo'>
-            <img src={MyLogo} alt="" />
+            <img src="images/logo.png" alt="" />
             <h3>Find your perfect trip, designed by insiders who know and love their cities!"</h3>
         </div>
         
