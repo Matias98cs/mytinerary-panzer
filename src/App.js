5b7792db59880fbad861939cc6165c01b15f592c
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-08-22 18:20:23 -0300

    Modificamos onClick

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Welcome from './pages/Welcome';
 import Carousel from './components/Carousel';
 import WebsiteLayout from './layouts/WebsiteLayout';
 import EventsCarousel from './components/EventsCarousel';
+import Cities from './components/Cities';
+import NewCityLayout from './layouts/NewCityLayout';
 
 
 function App() {
   return (
     <>
-      <EventsCarousel />
+      {/* <EventsCarousel /> */}
       <Welcome/>
-      <WebsiteLayout/>
+      {/* <Cities /> */}
+      <WebsiteLayout />
+      {/* <NewCityLayout /> */}
     </>
   );
 }
