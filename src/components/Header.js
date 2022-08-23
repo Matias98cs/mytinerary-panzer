431d49071a65a5975d61a314778c99a023453cde
Matias98cs <matias98cs@gmail.com>
2022-08-23 16:51:56 -0300

    se agrego mas estilos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import {Link as LinkRouter} from 'react-router-dom'
-import '../style/Header.css'
+import React from "react";
+import { Link as LinkRouter } from "react-router-dom";
+import "../style/Header.css";
 
 const page = [
-    {name:'Home', to:'/'},
-    {name:'Cities', to: '/cities'},
-]
-const link = (page)=> <LinkRouter className='Header-Link' to={page.to}>{page.name} </LinkRouter>
+  { name: "Home", to: "/" },
+  { name: "Cities", to: "/cities" },
+];
+const link = (page) => (
+  <LinkRouter className=" btnHover" to={page.to}>
+    {page.name}
+  </LinkRouter>
+);
 
 export default function Header() {
-    
-    return (
-        <header>
-            <div className='Header-logo'>
-            <img src="images/logo.png" alt="" />    
-            </div>
-            <div className='Header-link'>
-            {page.map(link)}
-            <div className='Header-login'>
-            <img src='./images/pngegg.png' alt=''/>       
-            </div>
-            </div>
-        </header>
-    )
-    }
+  return (
+    <header>
+      <div className="Header-logo">
+        <img src="images/logo.png" alt="" />
+        <h2>My Tinerary</h2>
+      </div>
+      <div className="Header-link">
+        {page.map(link)}
+        <div className="Header-login">
+          <img src="./images/pngegg.png" alt="" />
+        </div>
+      </div>
+    </header>
+  );
+}
