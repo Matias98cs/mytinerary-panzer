f1f30ce22f6b8eb3160d8ff110ec6b8cf986dc8b
Matias98cs <matias98cs@gmail.com>
2022-08-23 13:11:09 -0300

    agregamos y sacamos algunos componentes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,13 @@ import Welcome from './pages/Welcome';
 import Carousel from './components/Carousel';
 import WebsiteLayout from './layouts/WebsiteLayout';
 import EventsCarousel from './components/EventsCarousel';
-import Cities from './components/Cities';
-import NewCityLayout from './layouts/NewCityLayout';
 
 
 function App() {
   return (
-    <>
-      {/* <EventsCarousel /> */}
-      <Welcome/>
-      {/* <Cities /> */}
-      <WebsiteLayout />
-      {/* <NewCityLayout /> */}
-    </>
+      <WebsiteLayout>
+        <Welcome/>
+      </WebsiteLayout>
   );
 }
 
