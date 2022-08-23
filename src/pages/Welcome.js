a14f056947caaba7e6c56a3c91725999982185bf
Matias98cs <matias98cs@gmail.com>
2022-08-23 13:14:36 -0300

    agregamos el calltoaction a welcome

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction'
 import '../style/Welcome.css'
 
 export default function Welcome() {
@@ -9,8 +10,8 @@ export default function Welcome() {
             <img src="images/logo.png" alt="" />
             <h3>Find your perfect trip, designed by insiders who know and love their cities!"</h3>
         </div>
-        
-        <a href='#'>Login</a>
+        <CallToAction />
     </div>
+    
   )
 }
