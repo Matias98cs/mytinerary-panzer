e85b8f7a5eb19c9237c3baaecd4712f593b4bdb3
Matias98cs <matias98cs@gmail.com>
2022-08-23 15:21:48 -0300

    pasamos props al componente calltoaction

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -10,7 +10,7 @@ export default function Welcome() {
             <img src="images/logo.png" alt="" />
             <h3>Find your perfect trip, designed by insiders who know and love their cities!"</h3>
         </div>
-        <CallToAction />
+        <CallToAction linkto='cities' btnText={'Enter!'}/>
     </div>
     
   )
