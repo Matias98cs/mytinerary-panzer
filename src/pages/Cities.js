f25433a3aa0a611a6937c3b16355fd46ff44a579
Matias98cs <matias98cs@gmail.com>
2022-08-23 13:15:12 -0300

    creamos la pagina de cities

---
+++ src/pages/Cities.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Carousel from '../componets/Carousel'
+
+export default function Cities() {
+  return (
+    <div>
+        <h1>Popular MYtineraries</h1>
+        <Carousel />
+    </div>
+  )
+}
