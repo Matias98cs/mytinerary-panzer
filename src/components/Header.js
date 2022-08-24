2add4815a1c72a5a8e967896d7a703de63168bbe
Matias98cs <matias98cs@gmail.com>
2022-08-24 15:43:45 -0300

    cambiamos de lugar el avatar con su dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "../style/Header.css";
 import Menu from "./menu_hambur/Menu";
 
 
 export default function Header() {
-
-  const [open, setOpen] = useState(false)
-
-  const handleOpenMenu = () => {
-    if(open === true) {
-      setOpen(false)
-      console.log("open")
-    } else {
-      setOpen(true)
-      console.log("close")
-
-    }
-  }
-
 
   return (
     <header>
@@ -25,25 +11,7 @@ export default function Header() {
         <img src="images/logo.png" alt="" />
         <h2>My Tinerary</h2>
       </div>
-        {/* <div className="Header-login">
-          <img onClick={handleOpenMenu} src="./images/pngegg.png" alt="" />
-        </div> */}
-      
-      <div className="Header-dropdown">
-          {
-            open
-                ?
-                <ul>
-                  <li>Profile</li>
-                  <li>Log In</li>
-                </ul>
-                :
-                null
-          }
-        </div>
-      
         <Menu />  
-        
     </header>
   );
 }
