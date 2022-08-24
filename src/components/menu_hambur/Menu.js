ab8eaf0409eeb76bbe652e5f54926efbe926975f
Matias98cs <matias98cs@gmail.com>
2022-08-24 15:44:15 -0300

    colocamos el avatar en el menu

--- src/components/menu_hambur/Menu.js
+++ src/components/menu_hambur/Menu.js
@@ -1,36 +1,57 @@
-import React from 'react'
-import '../../style/MenuHambur.css'
+import React, { useState } from "react";
+
+import "../../style/MenuHambur.css";
 import { Link as LinkRouter } from "react-router-dom";
 
 const page = [
-    { name: "Home", to: "/" },
-    { name: "Cities", to: "/cities" },
-    { name: "New City", to: "/newcity" },
-
+  { name: "Home", to: "/" },
+  { name: "Cities", to: "/cities" },
+  { name: "New City", to: "/newcity" },
 ];
 const link = (page) => (
-    <LinkRouter className="nav_item" to={page.to}>
-        {page.name}
-    </LinkRouter>
+  <LinkRouter className="nav_item" to={page.to}>
+    {page.name}
+  </LinkRouter>
 );
 
 export default function Menu() {
-    return (
-        <>
-            <nav class="nav">
-                <div class="nav_container">
-                    <label for="menu" class="nav_label">
-                        <img src="./images/menu.svg" class="nav_img" alt="menu_logo" />
-                    </label>
-                    <input type="checkbox" id="menu" class="nav_input" />
+  const [open, setOpen] = useState(false);
 
-                    <div className="nav_menu">
-                        {page.map(link)}
-                    </div>
+  const handleOpenMenu = () => {
+    if (open === true) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
 
-                </div>
+  return (
+    <>
+      <nav className="nav">
+        <div className="nav_container">
+          <label htmlFor="menu" className="nav_label">
+            <img src="./images/menu.svg" className="nav_img" alt="menu_logo" />
+          </label>
+          <input type="checkbox" id="menu" className="nav_input" />
 
-            </nav>
-        </>
-    )
+          <div className="nav_menu">{page.map(link)}</div>
+          <div className="Header-dropdown">
+          {
+            open
+                ?
+                <ul>
+                  <li>Profile</li>
+                  <li>Log In</li>
+                </ul>
+                :
+                null
+          }
+        </div>
+          <div className="Header-login">
+            <img onClick={handleOpenMenu} src="./images/pngegg.png" alt="" />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
