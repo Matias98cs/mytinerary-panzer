636e8218680504a3f97c849717fbd752524bd87a
Matias98cs <matias98cs@gmail.com>
2022-08-30 18:53:26 -0300

    se uso fetch con axios en cards

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,107 +1,116 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../style/CityCards.css";
 import {Link as LinkRouter} from 'react-router-dom'
+import axios from "axios";
 
 function CityCard() {
 
-  let dataCities = [
-    {
-      photo: "./images/bariloche.jpg",
-      city: "Bariloche",
-      id: 1,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/paris1.jpg",
-      city: "Paris",
-      id: 2,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/valencia.jpg",
-      city: "Valencia",
-      id: 3,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/Cancun.jpg",
-      city: "Cancun",
-      id: 4,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/egypto.jpg",
-      city: "Egypto",
-      id: 5,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/tokio.jpg",
-      city: "Tokio",
-      id: 6,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/abu_dabi.jpg",
-      city: "Abu Dabi",
-      id: 7,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/camboya.jpg",
-      city: "Camboya",
-      id: 8,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/rio_de_janeiro.jpeg",
-      city: "Río de Janeiro",
-      id: 9,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/dublin.jpg",
-      city: "Dublín",
-      id: 10,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/sidney.jpg",
-      city: "Sidney",
-      id: 11,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-    {
-      photo: "./images/mallorca.jpg",
-      city: "Mallorca",
-      id: 12,
-      country: "",
-      population: 0,
-      fundation: 0
-    },
-  ];
+  // let dataCities = [
+  //   {
+  //     photo: "./images/bariloche.jpg",
+  //     city: "Bariloche",
+  //     id: 1,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/paris1.jpg",
+  //     city: "Paris",
+  //     id: 2,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/valencia.jpg",
+  //     city: "Valencia",
+  //     id: 3,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/Cancun.jpg",
+  //     city: "Cancun",
+  //     id: 4,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/egypto.jpg",
+  //     city: "Egypto",
+  //     id: 5,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/tokio.jpg",
+  //     city: "Tokio",
+  //     id: 6,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/abu_dabi.jpg",
+  //     city: "Abu Dabi",
+  //     id: 7,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/camboya.jpg",
+  //     city: "Camboya",
+  //     id: 8,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/rio_de_janeiro.jpeg",
+  //     city: "Río de Janeiro",
+  //     id: 9,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/dublin.jpg",
+  //     city: "Dublín",
+  //     id: 10,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/sidney.jpg",
+  //     city: "Sidney",
+  //     id: 11,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  //   {
+  //     photo: "./images/mallorca.jpg",
+  //     city: "Mallorca",
+  //     id: 12,
+  //     country: "",
+  //     population: 0,
+  //     fundation: 0
+  //   },
+  // ];
+
+  const [cities, setCities] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/cities/')
+      .then(response => setCities(response.data.response))
+  }, [])
+
 
   const cardCity = (city) => {
     return (
@@ -128,7 +137,7 @@ function CityCard() {
   return (
     <>
         <div className="cityCard-container">
-            {dataCities.map(cardCity)}
+            {cities.map(cardCity)}
         </div>
     </>
   )
