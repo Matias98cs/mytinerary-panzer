1263b679ab7c2ae6f2c5cdc3194a64f10988d8a8
Matias98cs <matias98cs@gmail.com>
2022-08-31 17:23:41 -0300

    se llamo al componente cardAndFind en Cities

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import CityCard from '../components/CityCard'
-import Input from '../components/Input'
+import CardsAndFind from '../components/CardsAndFind'
 import '../style/Cities.css'
 
 export default function Cities() {
 
   return (
     <div className="Cities-container">
-      <div>
-        <Input />
-      </div>
-      <CityCard />
+      <CardsAndFind/>
     </div>
   )
 }
