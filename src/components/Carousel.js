b547e92f26b3aed6b039c8e4b3fd35cf1f96ea45
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-08-31 13:32:01 -0300

    Consumimos la API

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,6 +3,7 @@ import "../style/Carousel.css";
 import Arrow from "./Carousel/Arrow";
 
 export default function Carousel(props) {
+
   const range = props.range
   const limitSlides = (props.slides * range)
   const [start, setStart] = useState(0)
@@ -12,15 +13,15 @@ export default function Carousel(props) {
   const time = props.interval * 1000
 
   const cityAndName = (item) => (
-    <div className="Carousel-card" key={item.id}>
-      <img src={item.url} alt={item.title}/>
-      <p>{item.title}</p>
+    <div className="Carousel-card" key={item._id}>
+      <img src={item.photo} alt={item.city} />
+      <p>{item.city}</p>
     </div>
   );
 
-  useEffect( () => {
-    let id = setInterval( function () {
-        next()
+  useEffect(() => {
+    let id = setInterval(function () {
+      next()
     }, time)
 
     setIntervalId(id)
@@ -32,7 +33,7 @@ export default function Carousel(props) {
     if (start >= range) {
       setStart(start - range);
       setEnd(end - range);
-    }else {
+    } else {
       setStart(limitSlides - range)
       setEnd(limitSlides)
     }
@@ -40,6 +41,7 @@ export default function Carousel(props) {
   }
 
   function next() {
+
     if (start < limitSlides - range) {
       setStart(start + range);
       setEnd(end + range);
@@ -59,7 +61,7 @@ export default function Carousel(props) {
       <div className="Carousel-container">
         <Arrow icon={"./images/arrow-left.svg"} click={previus} />
         <div className="Carousel-photo">
-        {photos.slice(start, end).map(cityAndName)}  
+          {photos.slice(start, end).map(cityAndName)}
         </div>
         <Arrow icon={"./images/arrow-right.svg"} click={next} />
       </div>
