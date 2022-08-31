0c331afb7d65740546400f8d60e459a562a04a26
Matias98cs <matias98cs@gmail.com>
2022-08-31 17:22:36 -0300

    se borro el componente cityCard

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,26 @@
 import React from "react";
 import '../style/Cities.css'
 
-export default function Input() {
+export default function Input({inputSearch}) {
+
+  const takeValue = (e) => {
+    const search = e.target.value
+    inputSearch(upperCaseOne(search))
+  }
+
+  function upperCaseOne(input){
+    return input.charAt(0).toUpperCase() + input.slice(1)
+  }
   return (
-    <div className="form-input-text">
-      <input type="text"  id="input"  placeholder="Find city" />
-    </div>
+    <>
+      <div className="form-input-text">
+        <input 
+          type="text"  
+          id="input"  
+          placeholder="Find city" 
+          onChange={takeValue}
+          />
+      </div>
+    </>
   );
 }
