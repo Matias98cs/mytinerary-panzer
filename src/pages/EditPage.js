811fb02ac866122642b4af55f0f03a0c8bcfd1ce
Matias98cs <matias98cs@gmail.com>
2022-09-01 08:39:28 -0300

    se creo la pagina de editPage

---
+++ src/pages/EditPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import EditForm from '../components/editCity/EditForm'
+import '../style/EditPage.css'
+
+export default function EditPage() {
+  return (
+    <div className='EditPage-container'>
+      <h1>Hello Here</h1>
+      <EditForm />
+    </div>
+  )
+}
