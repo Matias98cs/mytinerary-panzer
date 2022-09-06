fa7de7b8d2c24b7b4073dfd827d93eba1c9b5e41
Matias98cs <matias98cs@gmail.com>
2022-09-05 23:46:32 -0300

    se importo los actions de update y get para traer las cities y actualizar

--- src/components/editCity/EditForm.js
+++ src/components/editCity/EditForm.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Selects from './Selects'
+import InputEdit from './InputEdit'
+import '../../style/FormEditCity.css'
+import { useGetUpdateCityMutation, useGetCityByIdQuery } from '../../features/citiesAPI'
 
 function EditForm() {
+  const formRef = useRef();
+  const [valueSelect, setValueSelect] = useState()
+  const [valueData, setValueData] = useState([]);
+  const {data: city} = useGetCityByIdQuery(valueSelect)
+  const [UpdateNewCity] = useGetUpdateCityMutation()
+
+  useEffect(() => {
+    UpdateNewCity(valueData)
+      .unwrap()
+      .then(resp => console.log(resp))
+      .catch(error => console.log(error))
+  }, [valueData])
+
+
+  function takeValueSelect(value){
+    setValueSelect(value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const formCities = document.querySelector("#form-cities");
+
+    const forData = new FormData(formRef.current);
+    const values = Object.fromEntries(forData);
+    setValueData(values)
+    formCities.reset();
+  }
+  
   return (
-    <form>
-        <Selects />
+    <form 
+      className='Form-editCity'
+      id="form-cities"
+      onSubmit={handleSubmit}
+      ref={formRef}
+      >
+        <Selects  takeValueSelect={takeValueSelect}/>
+        {
+          city 
+          ? 
+          <InputEdit city={city} />
+          :
+          null
+        }
+        <button type='submit'>Edit City</button>
     </form>
   )
 }
