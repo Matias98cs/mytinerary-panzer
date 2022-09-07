6b686e66596ea8d1107d48f66838ea39e910ee46
Matias98cs <matias98cs@gmail.com>
2022-09-07 11:52:54 -0300

    se importo el itineraryAPI en el store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import citiesAPI from './features/citiesAPI'
+import itineraryAPI from './features/itineraryAPI';
 
 export default configureStore({
     reducer: {
-        [citiesAPI.reducerPath] : citiesAPI.reducer
+        [citiesAPI.reducerPath] : citiesAPI.reducer,
+        [itineraryAPI.reducerPath] : itineraryAPI.reducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(citiesAPI.middleware)
   })
