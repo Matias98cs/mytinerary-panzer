81ab7744f08d55c29ae4dd2a82230c728d2a79b6
Matias98cs <matias98cs@gmail.com>
2022-09-07 11:18:30 -0300

    se agrego los datos hardcodeados de un itinerario

---
+++ src/components/Itinerary.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Itinerary() {
+  return (
+    <div>
+        <p>Name: Caminata por ahi</p>
+        <p>user: Matias</p>
+        <p>city: Cordoba</p>
+        <p>price: $5</p>
+        <p>likes: 54</p>
+        <p>tags: #Matias, #javier</p>
+        <p>duration: 120</p>
+    </div>
+  )
+}
+
+export default Itinerary
