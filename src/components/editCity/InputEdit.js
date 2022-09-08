66470c902c7e1cf659f8cd3fbd7a04c6f6e16782
Matias98cs <matias98cs@gmail.com>
2022-09-08 00:56:41 -0300

    se cambio los estilos del componente inputEdit

--- src/components/editCity/InputEdit.js
+++ src/components/editCity/InputEdit.js
@@ -2,29 +2,33 @@ import React from "react";
 
 function InputEdit({ city }) {
   return (
-    <>
-     
-      <input name="_id" defaultValue={city?._id} />
-      <input type="text" name="city" defaultValue={city?.city} />
-      <input type="text" name="country" defaultValue={city?.country} />
-      <input type="text" name="photo" defaultValue={city?.photo} />
-      <input
-        type="number"
-        name="population"
-        defaultValue={city?.population}
-      />
-      <input
-        type="date"
-        name="fundation"
-        defaultValue={city?.fundation ? new Date(city?.fundation).toISOString().substring(0,10) : null}
-      />
-       <input
-        rows="4"
-        cols="50"
-        name="description"
-        defaultValue={city?.description}
-        ></input>
-    </>
+    <div className="container-inputs">
+      <input id="hide-id" name="_id" defaultValue={city?._id} />
+      <label htmlFor={city?.city}>
+        City
+        <input type="text" name="city" defaultValue={city?.city} id={city?.city} />
+      </label>
+      <label  htmlFor={city?.country}>
+        Country
+        <input type="text" name="country" defaultValue={city?.country} id={city?.country} />
+      </label>
+      <label>
+        Photo
+        <input type="text" name="photo" defaultValue={city?.photo} />
+      </label>
+      <label>
+        Population
+        <input type="number" name="population" defaultValue={city?.population}/>
+      </label>
+      <label>
+        Fundation
+        <input type="date" name="fundation" defaultValue={city?.fundation ? new Date(city?.fundation).toISOString().substring(0,10) : null}/>
+      </label>
+      <label>
+        Description
+        <input id="input-description" name="description" defaultValue={city?.description}/>
+      </label>
+    </div>
   );
 }
 
