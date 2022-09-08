37fdb05e3068974ebf6c45ebd673328ad2c33208
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-07 21:01:57 -0300

    Agregamos clases para estilos

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Activity from "./Activity";
 import Comments from "./Comments";
+import "../style/Itinerary.css"
+
 
 function Itinerary({ item }) {
 
   return (
-    <>
-      <div key={item._id}>
-        <p>Name:{item.name}</p>
-        <p>Duration:{item.duration}</p>
-        <p>Price: ${item.price}</p>
+    <div className="itinerary-container">
+      <h2>Itinerary</h2>
+      <div className="itinerary-title" key={item._id}>
+        <p>{item.name}</p>
       </div>
       <Activity data={item._id} />
-    </>
+      <div className="itinerary-price">  
+        <p>Duration: {item.duration} hours</p>
+        <p>Price: ${item.price}</p>
+      </div>       
+    </div>
   );
 }
 
