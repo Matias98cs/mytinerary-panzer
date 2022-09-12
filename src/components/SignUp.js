17b55406e8478df22770c0b78c0475906f4d0538
Matias98cs <matias98cs@gmail.com>
2022-09-12 18:34:53 -0300

    se creo la pagina de sigup

---
+++ src/components/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignUpGoogle from './SignUpGoogle'
+
+function SignUp() {
+  return (
+    <div>
+        <h2>Sign Up</h2>
+        <SignUpGoogle />
+    </div>
+  )
+}
+
+export default SignUp
