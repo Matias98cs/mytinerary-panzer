cffded525ed2acddbdfdb08ae2aa67b9a0ab5516
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-13 18:50:27 -0300

    LLamamos al usersAPI

--- src/store.js
+++ src/store.js
@@ -3,15 +3,17 @@ import citiesAPI from './features/citiesAPI'
 import itineraryAPI from './features/itineraryAPI';
 import actitiesAPI from './features/activities.API';
 import commenstAPI from './features/commentsAPI';
+import usersAPI from './features/usersAPI';
 
 export default configureStore({
     reducer: {
-        [citiesAPI.reducerPath] : citiesAPI.reducer,
-        [itineraryAPI.reducerPath] : itineraryAPI.reducer,
-        [actitiesAPI.reducerPath] : actitiesAPI.reducer,
-        [commenstAPI.reducerPath] : commenstAPI.reducer
+        [citiesAPI.reducerPath]: citiesAPI.reducer,
+        [itineraryAPI.reducerPath]: itineraryAPI.reducer,
+        [actitiesAPI.reducerPath]: actitiesAPI.reducer,
+        [commenstAPI.reducerPath]: commenstAPI.reducer,
+        [usersAPI.reducerPath]: usersAPI.reducer
 
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(citiesAPI.middleware)
-  })
+})
 
