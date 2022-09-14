bbd4081193a086fd4ded861e3fa1699685b0feda
Matias98cs <matias98cs@gmail.com>
2022-09-14 18:33:26 -0300

    se cambio el mensaje

--- src/components/InputsNewCity.js
+++ src/components/InputsNewCity.js
@@ -30,7 +30,7 @@ export default function InputsNewCity() {
     const forData = new FormData(formRef.current);
     const values = Object.fromEntries(forData);
     if(values.city == "" || values.country == "" || values.population == "" || values.fundation == "" || values.photo == ""){
-      setError('Algun campo esta vacio')
+      setError('Please enter all data')
     }else{
       sendCity(values);
     }
