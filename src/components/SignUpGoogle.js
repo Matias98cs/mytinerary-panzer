0d80b0ca935acfbccba588ffd47a2e12a6d1d81d
Matias98cs <matias98cs@gmail.com>
2022-09-15 15:05:56 -0300

    se reseteo el formualrio

--- src/components/SignUpGoogle.js
+++ src/components/SignUpGoogle.js
@@ -1,24 +1,34 @@
 import React, { useEffect, useRef } from "react";
 import * as jose from "jose";
+import { useGetPostNewUserMutation } from "../features/usersAPI";
 
 function SignUpGoogle() {
   const btnDiv = useRef(null);
+  const [addNewUser] = useGetPostNewUserMutation()
 
   async function handleCredentialResponse(response){
     let userObject = jose.decodeJwt(response.credential)
 
     let data = {
         name: userObject.name,
+        lastname: userObject.family_name,
         photo: userObject.picture,
-        email: userObject.email,
-        pass: userObject.sub,
+        mail: userObject.email,
+        country: "Argentina",
+        password: userObject.sub,
         role: 'user',
         from: 'google'
     }
-
     console.log(data)
+    sendNewUser(data)
   }
 
+  function sendNewUser(data) {
+    addNewUser(data)
+      .unwrap()
+      .then(succes => console.log(succes))
+      .catch(error => console.log(error))
+  }
   useEffect(() => {
         /* global google */
       google.accounts.id.initialize({
