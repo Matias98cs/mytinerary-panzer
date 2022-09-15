9b9c33c5bfc4bcb0951f6fbdbd7e6bda8ec4b699
Matias98cs <matias98cs@gmail.com>
2022-09-15 15:06:46 -0300

    se loguea el usuario por formulario y por google

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,31 +1,57 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import SingInGoogle from "../components/SignInGoogle";
 import "../style/SignIn.css";
-
+import { useGetSignInUserMutation } from "../features/usersAPI";
+import Alerts from "../components/Alert/Alerts";
 
 function SignIn() {
-    return (
-        <div className="SignIn-container">
-            <h2 className="signin-title">Sign In</h2>
-            <form>
-                <div className="signIn-inputs">
-                    <label>
-                        Email
-                        <input/>
-                    </label>
-                    <label>
-                        Password
-                        <input/>
-                    </label>
-                </div>
-                <div className="signIn-btn">
-                    <button>Enter</button>
-                </div>
-            </form>
-            <SingInGoogle />
+  const formData = useRef(null);
+  const [signinForm] = useGetSignInUserMutation();
+  const [error, setError] = useState("")
+
+  function singInUser(values) {
+    signinForm(values)
+        .unwrap()
+        .then(succes => {
+          setError("Welcome")
+        })
+        .catch(error => setError(error.data.message))
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const formSignUp = document.querySelector("#singin");
+    const forData = new FormData(formData.current)
+    const values = Object.fromEntries(forData)
+    values.from = 'form'
+    if(values.mail == "" || values.pass == ""){
+      setError("Please enter all data")
+    }else{
+      singInUser(values)
+      formSignUp.reset()
+    }
+  }
+
+  return (
+    <div className="SignIn-container">
+      <h2 className="signin-title">Sign In</h2>
+      <form id="singin" className="form-signin" ref={formData} onSubmit={handleSubmit}>
+        <label >
+          Email
+          <input name="mail" type="email"/>
+        </label>
+        <label >
+          Password
+          <input name="pass" type="password"/>
+        </label>
+        <div className="signIn-btn">
+          <button type="submit">Enter</button>
         </div>
-    );
+      </form>
+      <SingInGoogle />
+      <Alerts error={error} />
+    </div>
+  );
 }
 
-
-export default SignIn
+export default SignIn;
