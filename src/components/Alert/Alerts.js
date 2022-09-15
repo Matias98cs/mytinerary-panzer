01c39e82fc73e0b55053e2d915da53d4029f6639
Matias98cs <matias98cs@gmail.com>
2022-09-15 10:10:36 -0300

    se le agrego las clases los divs

--- src/components/Alert/Alerts.js
+++ src/components/Alert/Alerts.js
@@ -1,25 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import './Alerts.css'
 
 function Alerts({error}) {
     const [msg,setMsg] = useState("")
-
     useEffect(() => {
         setMsg(error)
     }, [error])
     
     setTimeout(() => {
         setMsg("")
-    }, 3000)
+    }, 4000)
   return (
-    <div>
+    <>
         {
             msg
             ?
-            <p>{msg}</p>
+            <div className='Container-message' id="msjAnimation">
+                <p id='msj'>{msg}</p>
+            </div>
             :
             null
         }
-    </div>
+    </>
   )
 }
 
