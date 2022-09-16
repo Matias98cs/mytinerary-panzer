2d889bb7a9a2eae0958a8c8ff4644d9edb6f1bfe
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-16 17:46:44 -0300

    Modificamos renderizado del componenteMyTinerary

--- src/pages/MyTineraries.js
+++ src/pages/MyTineraries.js
@@ -3,11 +3,11 @@ import { useParams } from "react-router-dom";
 import MyTinerary from "../components/MyTinerary";
 
 export default function MyTineraries() {
-const {id} = useParams()
+
 
     return (
         <div className="MyTinerary-container">
-            <MyTinerary id={id} />
+            <MyTinerary />
         </div>
     )
 }
