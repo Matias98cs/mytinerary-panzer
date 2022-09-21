e8eceac5d37e3d5ba4574d909a7547bfe2c62d94
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-21 16:08:54 -0300

    Agregamos endpoint para editar y eliminar

--- src/features/itineraryAPI.js
+++ src/features/itineraryAPI.js
@@ -10,9 +10,26 @@ export const itineraryAPI = createApi({
     endpoints: (builder) => ({
         getAllItineraries: builder.query({
             query: (cityId) => `/itineraries/query?city=${cityId}`
+        }),
+        deleteItinerary: builder.mutation({
+            query: (idItinerary) => ({
+                url:'/itineraries/' + idItinerary,
+                method: 'DELETE',
+            }) 
+        }),
+        getItinerary: builder.query ({
+            query: (id) => `http://localhost:4000/itineraries/finditinerary/${id} `
+        }),
+        updateItinerary: builder.mutation({
+            query:(playload) =>({
+                url:`http://localhost:4000/itineraries/${playload._id}`,
+                method: "PATCH",
+                body: playload
+            })
         })
     })
+
 })
 
 export default itineraryAPI;
-export const {useGetAllItinerariesQuery} = itineraryAPI
+export const {useGetAllItinerariesQuery, useDeleteItineraryMutation, useGetItineraryQuery, useUpdateItineraryMutation} = itineraryAPI
