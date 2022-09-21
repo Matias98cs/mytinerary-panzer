20d802f1c4f8012cb0c60d297b8e0ace270e0bb3
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-09-21 16:26:34 -0300

    Se corrigio identacion

--- src/features/itineraryAPI.js
+++ src/features/itineraryAPI.js
@@ -5,7 +5,7 @@ export const itineraryAPI = createApi({
     reducerPath: "itineraryAPI",
 
     baseQuery: fetchBaseQuery({
-        baseUrl:"http://localhost:4000/"
+        baseUrl: "http://localhost:4000/"
     }),
     endpoints: (builder) => ({
         getAllItineraries: builder.query({
@@ -13,33 +13,33 @@ export const itineraryAPI = createApi({
         }),
         deleteItinerary: builder.mutation({
             query: (idItinerary) => ({
-                url:'/itineraries/' + idItinerary,
+                url: '/itineraries/' + idItinerary,
                 method: 'DELETE',
-            }) 
+            })
         }),
-        getItinerary: builder.query ({
+        getItinerary: builder.query({
             query: (id) => `http://localhost:4000/itineraries/finditinerary/${id} `
         }),
         updateItinerary: builder.mutation({
-            query:(playload) =>({
-                url:`http://localhost:4000/itineraries/${playload._id}`,
+            query: (playload) => ({
+                url: `http://localhost:4000/itineraries/${playload._id}`,
                 method: "PATCH",
                 body: playload
             })
-
+        }),
         getPostNewItinerary: builder.mutation({
-            query(payload){
-                return{
-                    url: '/itineraries',
-                    method: 'POST',
-                    body: payload
+                query(payload) {
+                    return {
+                        url: '/itineraries',
+                        method: 'POST',
+                        body: payload
+                    }
                 }
-            }
+            })
         })
+
     })
 
-})
-
 export default itineraryAPI;
-export const {useGetAllItinerariesQuery, useDeleteItineraryMutation, useGetItineraryQuery, useUpdateItineraryMutation, useGetPostNewItineraryMutation} = itineraryAPI
+export const { useGetAllItinerariesQuery, useDeleteItineraryMutation, useGetItineraryQuery, useUpdateItineraryMutation, useGetPostNewItineraryMutation } = itineraryAPI
 
