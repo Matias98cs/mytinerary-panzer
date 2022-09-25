ac230057d8fcc2b1ebf3f7d4ae1a071d12b9bd74
Matias98cs <matias98cs@gmail.com>
2022-09-25 16:13:28 -0300

    cambios de identacion

--- src/components/MyTinerary.js
+++ src/components/MyTinerary.js
@@ -1,56 +1,59 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useGetAllItinerariesQuery, useItinerariesAllMutation } from "../features/myTineraryAPI";
+import {
+  useGetAllItinerariesQuery,
+  useItinerariesAllMutation,
+} from "../features/myTineraryAPI";
 import "../style/MyTinerary.css";
 import { Link as LinkRouter } from "react-router-dom";
 import { useDeleteItineraryMutation } from "../features/itineraryAPI";
-import {setReload} from '../features/likeSlice'
-import {setMessage} from '../features/messageSlice'
+import { setReload } from "../features/likeSlice";
+import { setMessage } from "../features/messageSlice";
 
 const MyTinerary = () => {
-  const [itineraryDelete] = useDeleteItineraryMutation()
-  const userId = useSelector(state => state.auth.userId)
-  const [itine, setItine] = useState([])
-  const [findItineraries] = useItinerariesAllMutation()
-  const dispatch = useDispatch()
-  const reload = useSelector(state => state.like.reload)
-  const user = useSelector(state => state.auth.user)
+  const [itineraryDelete] = useDeleteItineraryMutation();
+  const userId = useSelector((state) => state.auth.userId);
+  const [itine, setItine] = useState([]);
+  const [findItineraries] = useItinerariesAllMutation();
+  const dispatch = useDispatch();
+  const reload = useSelector((state) => state.like.reload);
+  const user = useSelector((state) => state.auth.user);
 
   async function getItireraries() {
     try {
-      let res = await findItineraries(userId)
-      if(res.data?.success){
-        setItine(res.data.response)
-      }else{
-        console.log(res.error.data)
+      let res = await findItineraries(userId);
+      if (res.data?.success) {
+        if (res.data?.response.length > 0) {
+          setItine(res.data.response);
+        } else {
+          setItine([]);
+        }
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
   async function deleteItinerary(key) {
     try {
-      let res = await itineraryDelete(key)
-      if(res.data?.success){
-        dispatch(setReload())
-        dispatch(setMessage({
-          message: 'Deleted itinerary',
-          success: true
-        }))
-      }else{
-        console.log(res.data)
-        
+      let res = await itineraryDelete(key);
+      if (res.data?.success) {
+        dispatch(setReload());
+        dispatch(
+          setMessage({
+            message: "Deleted itinerary",
+            success: true,
+          })
+        );
+      } else {
+        console.log(res.data);
       }
-    } catch (error) {
-      
-    }
+    } catch (error) {}
   }
 
   useEffect(() => {
-    getItireraries()
-  }, [reload, userId])
-  
+    getItireraries();
+  }, [reload, userId]);
 
   const showItinerary = (item) => {
     return (
@@ -67,8 +70,18 @@ const MyTinerary = () => {
             <p>Price: ${item?.price}</p>
             <p>{item?.tags}</p>
           </div>
-          <LinkRouter className="mytinerary-btn-edit" to={ `/edit-itinerary/${item?._id} `}>Edit itinerary</LinkRouter>
-          <button className="mytinerary-btn-delete" onClick={()=> deleteItinerary(item?._id)}>Delete</button>
+          <LinkRouter
+            className="mytinerary-btn-edit"
+            to={`/edit-itinerary/${item?._id} `}
+          >
+            Edit itinerary
+          </LinkRouter>
+          <button
+            className="mytinerary-btn-delete"
+            onClick={() => deleteItinerary(item?._id)}
+          >
+            Delete
+          </button>
         </div>
       </div>
     );
@@ -76,14 +89,10 @@ const MyTinerary = () => {
   return (
     <>
       {itine.length > 0 ? (
-        <> 
+        <>
           <div className="mytinerary-container">
             <h1>My Tineraries</h1>
-            <img
-              className="mytinerary-img"
-              src={user.photo}
-              alt="photo"
-            />
+            <img className="mytinerary-img" src={user.photo} alt="photo" />
             <div className="mytinerary-user">
               <div className="mytinerary-profile">
                 <h3>{user.name}</h3>
@@ -91,9 +100,7 @@ const MyTinerary = () => {
               </div>
             </div>
           </div>
-          <div className="mytinerary-general">
-            {itine?.map(showItinerary)}
-          </div>
+          <div className="mytinerary-general">{itine?.map(showItinerary)}</div>
         </>
       ) : (
         <div className="itinerarios-clear">
