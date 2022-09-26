7865a6000c8f0d5b90adbc17f6fdcc7248a7ca79
Matias98cs <matias98cs@gmail.com>
2022-09-26 12:14:50 -0300

    cambio de apiurl

--- src/features/citiesAPI.js
+++ src/features/citiesAPI.js
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import apiurl from '../api';
 
 export const citiesAPI = createApi({
     reducerPath: "citiesAPI",
 
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:4000/"
+        baseUrl: apiurl
 
     }),
     endpoints: (builder) => ({
